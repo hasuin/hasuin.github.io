ec6f7c9db4aa58b3336406173f607abed23e23bf
ChalkPE <chalk@chalk.pe>
2017-02-27 16:27:44 +0900

    webpack: extract css files

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,41 +1,58 @@
-const path = require('path');
-const webpack = require('webpack');
+import path from 'path';
+import webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 module.exports = {
     entry: './app/app.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
         publicPath: '/dist/',
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
             {
 				test:/\.vue$/,
 				loader: 'vue-loader',
-				options: { loaders: { html: 'pug-loader' } }
+				options: {
+                    loaders: {
+                        html: 'pug-loader',
+                        css: ExtractTextPlugin.extract({
+                            use: 'css-loader',
+                            fallback: 'vue-style-loader'
+                        })
+                    }
+                }
             }, {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
             }, {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: ExtractTextPlugin.extract({
+                    use: "css-loader",
+                    fallback: "style-loader"
+                })
             }, {
                 test: /\.(png|jpe?g|gif|svg|ttf|woff2?|eot)$/,
                 loader: 'file-loader',
-                options: { name: '[name].[ext]?[hash]' }
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
             }
         ]
     },
+    plugins: [
+        new ExtractTextPlugin('bundle.css')
+    ],
     resolve: {
         alias: {
             'vue$': 'vue/dist/vue.common.js'
         }
     },
     devServer: {
-        historyApiFallback: true,
-        host: '0.0.0.0', noInfo: true
+        host: '0.0.0.0',
+        historyApiFallback: true
     },
     performance: {
         hints: false
